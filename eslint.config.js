4991a015e6b40f328305595efe41d54c2e712eb7
Guillaume Loiseleux <guillaumeloiseleux@MBPdeGuillaume.lan>
2026-01-18 16:18:05 +0100

    chore: add Prettier configuration, update ESLint settings, and upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const reactCompiler = require("eslint-plugin-react-compiler");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
+  reactCompiler.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
   },
 ]);
